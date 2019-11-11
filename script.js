91e3d5d2b5215cd23fa8ad56035b3fb773bdbea5
Pete8219 <psavrasov@gmail.com>
2019-11-11 22:59:11 +0500

    add search Form

---
+++ script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () =>{
+   
+    
+    let closeButton = document.querySelector('.close_search'),
+        topSearch =  document.querySelector('.top__search'),
+        searchInput = document.querySelector('.search__input'),
+        searchBlock = document.querySelector('.search-block');
+
+
+    topSearch.addEventListener("click", function(e) {
+        if (event.target.className == 'close_search') {
+        topSearch.style.top = '-160px';
+        searchInput.value = '';
+    }
+    });
+
+    searchBlock.addEventListener("click", function(e) {
+       let target = event.target.closest('search-block');
+        if(target = event.target) {
+        topSearch.style.top = '0';
+        topSearch.style.transition = '2s easy-in-out';
+
+
+        }
+    });
+
+});
